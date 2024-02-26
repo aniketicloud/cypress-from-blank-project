d9af27e11207154846b4dd868f6138e7bc06150e
Aniket Pandharabale <aniketicloud@gmail.com>
2024-02-26 23:34:55 +0530

    added cypress-mochawesome-reporter and generated a report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from "cypress";
+import plugin from "cypress-mochawesome-reporter/plugin";
 
 export default defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "custom-title",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      // the following commented code gives an error with require statement
+      // require('cypress-mochawesome-reporter/plugin')(on)
+      plugin(on);
     },
   },
 });
--- cypress/e2e/trial.cy.ts
+++ cypress/e2e/trial.cy.ts
@@ -3,6 +3,7 @@ describe("template spec", () => {
     expect(true).to.be.true;
   });
   it("fails", () => {
+    cy.visit("https://about.deere.com")
     expect(true).to.be.false;
   });
 });
